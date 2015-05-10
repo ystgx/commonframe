0d6eaddd82b79687bce0fffc25411db3dc0dc2ea
ystgx <ystgx@outlook.com>
2015-05-11 00:00:30 +0800

    参考开源项目

--- commonframe/src/main/java/com/web/commonframe/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.web.commonframe;
-
-public class Constants {
-	public static final String CURRENT_USER = "user";
-	
-}
--- commonframe/src/main/java/com/web/commonframe/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.web.commonframe.service;
-
-import com.web.commonframe.entity.User;
-
-public interface UserService {
-	
-	User createUser(User user);
-}
